c0aa6e3ac1dca7ab5ca077bb6f0839ecf0f6fa52
Hakney <hakneey@gmail.com>
2021-02-26 15:09:29 -0300

    iniciando com nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const consign = require("consign");
+
+const PORT  = 3000;
+
+const app = express();
+
+// ajustar json
+app.set("json spaces", 4);
+
+consign()
+    .include("routes")
+    .into(app);
+
+app.listen(PORT, () =>  console.log(`NTASK - API -> porta ${PORT}`));
